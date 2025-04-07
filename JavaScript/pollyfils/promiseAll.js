dc51a012c074984a1cc4b916c571c6d612c701a8
Krishnendu Narayan <57310676+Krishnenu@users.noreply.github.com>
2025-04-08 01:15:17 +0530

    promise.all added

---
+++ JavaScript/pollyfils/promiseAll.js
@@ -0,0 +1,38 @@
+function makePromise() {
+  let promiseArray = [];
+  for (let i = 0; i < 2; i++) {
+    promiseArray.push(
+      new Promise((res, rej) => {
+        setTimeout(() => {
+          res(`Promise Resolved ${i}`);
+        }, 2000);
+      })
+    );
+  }
+  return promiseArray;
+}
+
+const promises = makePromise();
+
+/* Promise.all(promises).then((res)=>res.map((promise)=> console.log(promise))); */
+
+function customPromise(promises) {
+  return new Promise((resolve, reject) => {
+    let results = [];
+    let completed = 0;
+
+    promises.forEach((promise, index) => {
+      promise
+        .then((res) => {
+          results.push(res);
+          completed++;
+          if (completed === promises.length) {
+            resolve(results);
+          }
+        })
+        .catch((err) => reject(err));
+    });
+  });
+}
+
+console.log(customPromise(promises).then((res) => console.log(res)));
